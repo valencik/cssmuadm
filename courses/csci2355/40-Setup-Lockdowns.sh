113f2154d0dcbed9852594c812dbb383269a59c8
Andrew Valencik <andrew.valencik@gmail.com>
2014-10-13 23:38:02 -0300

    Added initial script for setting up lockdown procedures.
    Needs configuration echo.
    Needs more testing.
    Needs a repository for scripts.

---
+++ courses/csci2355/40-Setup-Lockdowns.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+#This script creates the cron job to lockdown student submission files.
+#It requires a $courseShortName.usrpasswd file exist in current directory
+
+#Configuration
+courseShortName=csc355
+path=/home/course/$courseShortName
+minAccount=01
+maxAccount=49
+instructor=$courseShortName"00"
+marker=$courseShortName"50"
+numberSubmissions=12
+lockdownSchedule=$path/lockdown/lockdownSchedule2014
+
+#TODO echo configuration data
+
+#Copy lockdown scripts to course lockdown folder
+mkdir -p $path/lockdown
+#cp ./lockdown.sh $path/lockdown/
+#cp ./lockdownSchedule* $path/lockdown/
+
+#Ensure lockdown cronjob exists
+crontab -u $instructor -l | grep -F \
+    "59 23 * * Sun sudo $path/lockdown/lockdown.sh $lockdownSchedule"
+grepReturn=$?
+if [ $grepReturn -eq 0 ]
+then
+    echo "Cronjob exists"
+else
+    echo "Creating lockdown cronjob!"
+    crontab -u $instructor -l > tmpCrontab.log
+    echo "59 23 * * Sun sudo $path/lockdown/lockdown.sh $lockdownSchedule" >> tmpCrontab.log
+    crontab -u $instructor tmpCrontab.log
+fi
+
+#Add lockdown script exceptions to sudoers list for csc35500
+if [ -f /etc/sudoers.d/csc355.conf ]
+then
+    echo "CSC355 sudoers exception exists"
+else
+    echo "Creating sudoers exception!"
+    echo "Cmnd_Alias  LOCKDOWN355=$path/lockdown/lockdown.sh, $path/lockdown/unlock.sh, $path/lockdown/lock.sh" >> /etc/sudoers.d/csc355.conf
+    echo "csc35500 ALL = (root) NOPASSWD: LOCKDOWN355" >> /etc/sudoers.d/csc355.conf
+fi
