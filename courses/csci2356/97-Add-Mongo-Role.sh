91cb420068d4dee97b87b62fad4f23cf6814182c
Andrew Valencik <andrew.valencik@gmail.com>
2015-03-11 12:57:53 -0300

    Added scripts to create/add new roles
    Current studentAdminActions role enables students to change their own password

---
+++ courses/csci2356/97-Add-Mongo-Role.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+# This script adds a mongo role to the users in the provided usr.passwd file
+# If the role does not already exist in the admin database, you must create it
+# mongo admin -p -u siteUserAdmin create-studentAdminActions-role.js
+
+#Check usage
+if [ $# -ne 2 ]; then
+  echo "ERROR: This script requires a user:password and a mongo role as parameters."
+  exit
+fi
+
+#Check the input file is valid
+if [ -f $1 ];
+then
+  usrpasswdfile=$1
+else
+  echo "ERROR: $1 is not valid"
+  exit
+fi
+
+# Confirm the role with the user
+echo "Going to add role $2 to all users in $1?"
+read -p "Press [Enter] key to continue or Ctrl+C to cancel..."
+
+# Generate the mongo-user.js file
+while IFS=: read -r user pass;
+do
+  echo "db = db.getSiblingDB('$user')" >> mongo-user-roles.js
+  echo "db.grantRolesToUser( \"$user\", [ { role: \"$2\", db: \"admin\" } ])" >> mongo-user-roles.js
+done < "$usrpasswdfile"
+
+# Print out instructions
+echo "Run mongo mongo-user-roles.js to make the user modifications."
