6f274c8a422e9a791251e7784c6c6e47153ba6d6
Sarah <rainbee2214@gmail.com>
2015-05-21 16:11:43 -0300

    Added script for initializing Parallel

---
+++ parallel/00-Initialize-Parallel-VM.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This script is testing installationa and setup on Ubuntu 14
+
+#Set hostname
+NEW_HOSTNAME=paulR
+hostnamectl set-hostname $NEW_HOSTNAME
+sed -i 's/127.0.1.1.*/127.0.1.1\t'"$NEW_HOSTNAME"'/g' /etc/hosts
+
+#Make sure loopback device has IP
+ip addr add 127.0.0.1 dev lo
+
+#Get and apply any updates
+apt-get update
+apt-get --assume-yes upgrade
+
+#Install basic tools
+apt-get --assume-yes --quiet install git vim ethtool iperf
+apt-get --assume-yes --quiet install p7zip-full
+apt-get --assume-yes --quiet install aspell
+
+apt-get --assume-yes --quiet install parallel
+
