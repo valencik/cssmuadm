28f5826e938da6e63ad461996968b58d20819b8b
Andrew Valencik <andrew.valencik@gmail.com>
2015-09-05 11:26:52 -0300

    Added mongo admin user siteUserAdmin

--- courses/cda5540/20-Setup-Mongo-Databases.sh
+++ courses/cda5540/20-Setup-Mongo-Databases.sh
@@ -2,6 +2,7 @@
 set -e
 
 # This script creates mongo users and enables mongod authentication
+mongoAdminPass=mySecretMongoAdminPassword
 
 #Check usage
 if [ $# -ne 1 ]; then
@@ -17,6 +18,10 @@ else
   echo "ERROR: $1 is not valid"
   exit
 fi
+
+# Create mongo user siteUserAdmin
+echo "db = db.getSiblingDB('admin')" >> mongo-users.js
+echo "db.createUser( { user: \"siteUserAdmin\", pwd: \"$mongoAdminPass\", roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\" } ] })" >> mongo-users.js
 
 # Generate the mongo-user.js file
 while IFS=: read -r user pass;
