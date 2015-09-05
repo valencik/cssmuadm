3fd2eb9408e594b77fe35069359a6c6794f25bdd
Andrew Valencik <andrew.valencik@gmail.com>
2015-09-05 12:12:25 -0300

    Updated to MongoDB 3.0

--- courses/cda5540/00-Initialize-CDA5540-VM.sh
+++ courses/cda5540/00-Initialize-CDA5540-VM.sh
@@ -138,7 +138,8 @@ then
 
   #Install MongoDB from mongo's packages
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+  echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
   sudo apt-get update
   apt-get --assume-yes --quiet install mongodb-org
+  sudo service mongod start
 fi
