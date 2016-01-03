f73e7f184ef20cf52e8b62ceeefeedde12f4fea1
Andrew Valencik <andrew.valencik@gmail.com>
2016-01-03 14:14:44 -0400

    Added prototype cs.dev server initialize script

---
+++ servers/README.md
@@ -0,0 +1,5 @@
+Servers
+=======
+
+This directory stores all the server configuration scripts that make use of the tasks help scripts.
+The server configurations in courses/ will be migrated to this format.
---
+++ servers/dev.cs/00-initialize-cs-dev.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+#Load configuration file
+source config.env
+
+#Get and apply any updates
+apt-get update
+apt-get --assume-yes upgrade
+
+#Change to project root directory
+cd ../../
+
+#Tasks
+source tasks/install-LAMP.sh
+source tasks/install-MongoDB.sh
+
