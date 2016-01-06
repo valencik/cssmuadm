9cf9505e046cc724c732e6e6f3f99c5dfc663c51
Andrew Valencik <andrew.valencik@gmail.com>
2016-01-06 09:24:07 -0400

    Skel dir copy must be recursive

--- servers/dev.cs/00-initialize-cs-dev.sh
+++ servers/dev.cs/00-initialize-cs-dev.sh
@@ -25,7 +25,7 @@ mkdir /etc/skel/public_html
 while IFS=: read -r pw_name pw_passwd pw_uid pw_gid pw_gecos pw_dir pw_shell;
 do
   if [ -d "$pw_dir" ]; then
-    cp /etc/skel/. "$pw_dir"
+    cp -r /etc/skel/. "$pw_dir"
   fi
 done < full.usrpasswd
 
