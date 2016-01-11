4d7914319b59dff8de902fbb0ca7c89e17eccb78
Andrew Valencik <andrew.valencik@gmail.com>
2016-01-11 16:01:08 -0400

    Fixed add-users script to use usrpasswd format

--- tasks/add-users.sh
+++ tasks/add-users.sh
@@ -2,39 +2,32 @@
 set -e
 
 #Check usage
-if [ $# -ne 2]; then
-  echo "ERROR: This script requires a path and user:password file as parameters."
-  echo "USAGE: ./$0 /home/faculty faculty.usrpasswd"
+if [ $# -ne 1]; then
+  echo "ERROR: This script requires a user:password file."
+  echo "USAGE: ./$0 student.usrpasswd"
   exit
 fi
-
-#Check the input path is valid
-path=$1
-if [ -d "$path" ];
-  echo "WARN: Path $path does not exist. Continuing will create it."
-  read -p "Press [Enter] key to continue or Ctrl+C to cancel..."
-  mkdir -p "$path"
-fi
-
 #Check the input file is valid
-if [ -f $2 ];
+if [ -f $1 ];
 then
-  usrpasswdfile=$2
+  usrpasswdfile=$1
 else
-  echo "ERROR: $2 is not valid"
+  echo "ERROR: $1 is not valid"
   exit
 fi
 
-while IFS=: read -r user pass;
+while IFS=: read -r pw_name pw_passwd pw_uid pw_gid pw_gecos pw_dir pw_shell;
 do
   #Ensure users exist and passwords match file
-  if id -u $user >/dev/null 2>&1; then
+  if id -u $pw_name >/dev/null 2>&1; then
     echo "User $user already exists. Continuing will update password."
     read -p "Press [Enter] key to continue or Ctrl+C to cancel..."
-    echo "$user:$pass" | chpasswd 
+    echo "$pw_name:$pw_passwd" | chpasswd 
   else
     #Create users in $path
-    useradd --base-dir $path --create-home \
-            --shell /bin/bash --password $(openssl passwd $pass) $user  
+    useradd --home $pw_dir --create-home --skel /etc/skel \
+            --shell $pw_shell --password $(openssl passwd $pass) $user  
+    fullname=$(echo "$pw_gecos" | sed s/,//g)
+    chfn -f "$fullname" $pw_name
   fi
 done < "$usrpasswdfile"
