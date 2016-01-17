94eaf1b0c9692d9db144736dac739032858c4434
Andrew Valencik <andrew.valencik@gmail.com>
2016-01-17 12:00:01 -0400

    No longer change user passwd if already exists

--- tasks/add-users.sh
+++ tasks/add-users.sh
@@ -20,9 +20,7 @@ while IFS=: read -r pw_name pw_passwd pw_uid pw_gid pw_gecos pw_dir pw_shell;
 do
   #Ensure users exist and passwords match file
   if id -u $pw_name >/dev/null 2>&1; then
-    echo "User $user already exists. Continuing will update password."
-    read -p "Press [Enter] key to continue or Ctrl+C to cancel..."
-    echo "$pw_name:$pw_passwd" | chpasswd 
+    echo "User $pw_name already exists. Not changing password."
   else
     #Create users in $path
     useradd --home-dir $pw_dir --create-home --skel /etc/skel \
